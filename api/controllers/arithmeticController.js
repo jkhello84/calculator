b01ebccaef652b3040febeaccdf04f5823821b41
jkhello84 <69764158+jkhello84@users.noreply.github.com>
2020-08-16 13:52:16 -0400

    Added comment before add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // + sign required to coerce variables into numbers and avoid string concate 
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
